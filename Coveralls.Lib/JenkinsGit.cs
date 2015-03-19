917369a3fcaa7becb064909c1cd535ea61f5130c
Jason Deering <jason@deering.me>
2015-03-19 10:48:51 -0500

    Adding tests to for new code Adding PullRequestId info in jenkins support Modifying jenkins support changes to fail gracefully

--- Coveralls.Lib/JenkinsGit.cs
+++ Coveralls.Lib/JenkinsGit.cs
@@ -12,23 +12,48 @@ namespace Coveralls
         {
             get
             {
-                if (string.IsNullOrEmpty(_branch)) _branch = Environment.GetEnvironmentVariable("GIT_BRANCH");
+                if (_branch.IsBlank())
+                {
+                    _branch = Environment.GetEnvironmentVariable("GIT_BRANCH");
+                    
+                    if (_branch.IsBlank()) _branch = _repository.Head.Name;
 
-                // For github...
-                // The git branch is of the form origin/<branch> or origin/pr/1152/merge.  The links on the coveralls page
-                // will link to that branch, but only the first form (without origin) will work.  Strip away all but the last
-                // element for the first cast and leave everything for the first one.
-
-                if (_branch.Contains("/")) {
-                    if (!(_branch.Contains("pr") || _branch.Contains("/merge"))) {
-                        int index = _branch.IndexOf("/") + 1;
-                        if(index < _branch.Length){
-                            _branch = _branch.Substring(index);
+                    // The git branch is of the form origin/<branch> or origin/pr/1152/merge.  The links on the coveralls page
+                    // will link to that branch, but only the first form (without origin) will work.  Strip away all but the last
+                    // element for the first cast and leave everything for the first one.
+                    if (_branch.Contains("/"))
+                    {
+                        if (_branch.Contains("pr")) _branch = "master";
+                        else
+                        {
+                            var index = _branch.IndexOf("/") + 1;
+                            if (index < _branch.Length)
+                            {
+                                _branch = _branch.Substring(index);
+                            }
                         }
                     }
                 }
 
                 return _branch;
+            }
+        }
+        
+        private CommitData _head;
+        public override CommitData Head
+        {
+            get
+            {
+                var branch = Environment.GetEnvironmentVariable("GIT_BRANCH");
+                _head = base.Head;
+                if (_head != null)
+                {
+                    if (branch.IsNotBlank() && branch.Contains("pr"))
+                    {
+                        _head.PullRequestId = branch.Split('/')[2];
+                    }
+                }
+                return _head;
             }
         }
     }
